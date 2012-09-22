fe661a5ceb4a152f5a87272fd93fbf22c2183e8e
Marco Koch <marco@5th.ch>
2012-09-22 11:40:17 +0200

    renamed & added 3V Referece

--- mqttTempC.ino
+++ mqttTempC.ino
@@ -7,8 +7,11 @@
 char* tempTopic = "byteli/temp/1";
 char* message = "LoL";*/
 
+#define aref_voltage 3.3
+
 int sensorPin = A0;
 int sensorValue = 0;
+int sensorOldValue = 0;
 //int celsius = 0;
 
 char sensorString[100];
@@ -27,31 +30,34 @@ PubSubClient client(server, 1883, callback);
 void setup()
 {
   Ethernet.begin(mac, ip);
-  client.connect("arduinoWill");
-  Serial.begin(9600);
+  client.connect("arduinoTempC");
+  analogReference(EXTERNAL);
   
 }
 
 void loop()
 {
   
-  //client.loop();
+  client.loop();
   
   sensorValue = analogRead(sensorPin);
   
-  float tempC = convertToTempC(sensorValue);
+  if (sensorValue - sensorOldValue != 0)
+  {
+    float tempC = convertToTempC(sensorValue);
+    dtostrf(tempC, 5, 2, sensorString);
+    client.publish("byteli/temp/1", sensorString); 
+  }
 
-  dtostrf(tempC, 5, 2, sensorString);
-
-  client.publish("byteli/temp/1", sensorString);
-  delay(2500);
+  sensorOldValue = sensorValue;
+  delay(1000);
  
 }
 
 float convertToTempC(int raw)
 {
   
-  float voltage = raw * 5.0;
+  float voltage = raw * aref_voltage;
   voltage /= 1024.0;
   
   float tempC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
