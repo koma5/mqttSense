30f8f9d8867ead2f50ea59c011b8f3684ea794c1
Marco Koch <marco@5th.ch>
2014-11-16 14:46:46 +0100

    dedicateed broker for VW-Büssli

--- mqttTempB.ino
+++ mqttTempB.ino
@@ -19,7 +19,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 // Update these with values suitable for your network.
 byte mac[]    = {  0x90, 0xA2, 0xDA, 0x0D, 0xB9, 0x1D };
-byte server[] = { 172, 16, 0, 70 };
+byte server[] = { 172, 16, 42, 70 };
 byte ip[]     = { 172, 16, 42, 36 };
 
 EthernetClient ethClient;
